3b79053967bd27c5f36f9df83dce88cec298e77a
Zerro613 <31075773+Zerro613@users.noreply.github.com>
2018-05-01 10:49:21 -0600

    Update final project.cpp

--- final project.cpp
+++ final project.cpp
@@ -9,9 +9,59 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	
 	int players = 0;
-	string playerOne = ""
+	string category = "";
+	string playerOne = "";
+	string playerTwo = "";
+	string playerThree = "";
+	string playerFour = "";
  	cout << "Welcome to our trivia game!" << endl;
-	cout << "Please enter the number of players" << endl;
+	cout << "Please enter a number of players (1 thorugh 4): ";
 	cin >> players;
 	
+	if (players == 1){
+	cout << "Please enter your name: ";
+	cin >> playerOne; 
+	cout << "Hello " << playerOne << endl; 
+		}
+	if (players == 2){
+		cout << "Please enter player one's name: ";
+		cin >> playerOne; 
+		cout << "Hello " << playerOne << endl;
+		cout << "Please enter player two's name: ";
+		cin >> playerTwo; 
+		cout << "Hello " << playerTwo << endl; 
+		}
+	if (players == 3){
+		cout << "Please enter player one's name: ";
+		cin >> playerOne; 
+		cout << "Hello " << playerOne << endl;
+		cout << "Please enter player two's name: ";
+		cin >> playerTwo; 
+		cout << "Hello " << playerTwo << endl;
+		cout << "Please enter player three's name: ";
+		cin >> playerThree; 
+		cout << "Hello " << playerThree << endl; 
+		}
+	if (players == 4){
+		cout << "Please enter player one's name: ";
+		cin >> playerOne; 
+		cout << "Hello " << playerOne << endl;
+		cout << "Please enter player two's name: ";
+		cin >> playerTwo; 
+		cout << "Hello " << playerTwo << endl;
+		cout << "Please enter player three's name: ";
+		cin >> playerThree; 
+		cout << "Hello " << playerThree << endl;
+		cout << "Please enter player four's name: ";
+		cin >> playerFour; 
+		cout << "Hello " << playerFour << endl; 
+		}	
+		else if (players != 1 || 2 || 3 || 4) {
+			cout << "Please enter a correct number (restart the program and try again)."; 
+		}	
+		cout << "please enter a category:
+			Movies
+			Video Games
+			or T.V. Shows";
+			cin >> category; 
 }
