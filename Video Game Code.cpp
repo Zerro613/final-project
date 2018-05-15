48b8c6dbdf993312d94bf8060b899cb3441a33bc
Zerro613 <31075773+Zerro613@users.noreply.github.com>
2018-05-15 08:12:04 -0600

    Update Video Game Code.cpp

--- Video Game Code.cpp
+++ Video Game Code.cpp
@@ -1,6 +1,50 @@
+/*
+Base Code
+Author: Owen Palmer
+Date: 5/1/18
+*/
+#include <string>
 #include <iostream>
 
 using namespace std;
 int main(int argc, char *argv[]) {
 	
+	//Declare values
+	int i = 0;
+	int playerPoint = 0;
+	int questionNum = 0;
+	string answer = "";
+	
+	//Declare questions & answers
+	string category[40][2] = {{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},{"Question\nA)_\nB)_\nC)_\nD)_","answer"},};
+	
+	//40 questions
+	while (i != 40) {
+
+		//Ask question
+		cout << category[i][0] << "\nAnswer: ";
+		cin >> answer;
+		
+		//Toupper
+		transform(answer.begin(), answer.end(), answer.begin(), ::tolower);
+		
+
+		//Check if answer is correct
+		if (answer == category[i][1]) {
+			cout << "\nCorrect!\n\n\n";
+			
+			//Give the player a point
+			playerPoint = playerPoint + 1;
+		}
+		else {
+			cout << "\nIncorrect answer, U Suk!\n\n\n";
+		}
+		//Next question/answer
+		i = i + 1;
+	}
+	//Display points
+	cout << "Your total points: " << playerPoint;
+	
+	//Thank user
+	cout << "\n\n\n\nThank you for using this program! :)";
 }
